27a06102674b97696b8d209d81ae30d233c7551d
Andy Herbert <andy.herbert@gmail.com>
2013-06-23 14:00:57 +0100

    The pointer length bit-width can now be supplied when compressing a file. Breaks compatibility with files compressed with the old method.

--- lz.c
+++ lz.c
@@ -1,47 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
-uint32_t lz77_compress (uint8_t *uncompressed_text, uint32_t uncompressed_size, uint8_t *compressed_text)
+uint32_t lz77_compress (uint8_t *uncompressed_text, uint32_t uncompressed_size, uint8_t *compressed_text, uint8_t pointer_length_width)
 {
-    uint8_t pointer_length, temp_pointer_length;
-    uint16_t pointer_pos, temp_pointer_pos, output_pointer;
+    uint16_t pointer_pos, temp_pointer_pos, output_pointer, pointer_length, temp_pointer_length;
     uint32_t compressed_pointer, output_size, coding_pos, output_lookahead_ref, look_behind, look_ahead;
+    uint16_t pointer_pos_max, pointer_length_max;
+    pointer_pos_max = pow(2, 16 - pointer_length_width);
+    pointer_length_max = pow(2, pointer_length_width);
 
     *((uint32_t *) compressed_text) = uncompressed_size;
-    compressed_pointer = output_size = 4;
-
+    *(compressed_text + 4) = pointer_length_width;
+    compressed_pointer = output_size = 5;
+    
     for(coding_pos = 0; coding_pos < uncompressed_size; ++coding_pos)
     {
         pointer_pos = 0;
         pointer_length = 0;
-        for(temp_pointer_pos = 1; (temp_pointer_pos < 4096) && (temp_pointer_pos <= coding_pos); ++temp_pointer_pos)
+        for(temp_pointer_pos = 1; (temp_pointer_pos < pointer_pos_max) && (temp_pointer_pos <= coding_pos); ++temp_pointer_pos)
         {
             look_behind = coding_pos - temp_pointer_pos;
             look_ahead = coding_pos;
             for(temp_pointer_length = 0; uncompressed_text[look_ahead++] == uncompressed_text[look_behind++]; ++temp_pointer_length)
-                if(temp_pointer_length == 15)
+                if(temp_pointer_length == pointer_length_max)
                     break;
             if(temp_pointer_length > pointer_length)
             {
                 pointer_pos = temp_pointer_pos;
                 pointer_length = temp_pointer_length;
-                if(pointer_length == 15)
+                if(pointer_length == pointer_length_max)
                     break;
             }
         }
         coding_pos += pointer_length;
-        if(pointer_length && (coding_pos == uncompressed_size))
+        if((coding_pos == uncompressed_size) && pointer_length)
         {
-            output_pointer = (pointer_pos << 4) | (pointer_length - 1);
+            output_pointer = (pointer_length == 1) ? 0 : ((pointer_pos << pointer_length_width) | (pointer_length - 2));
             output_lookahead_ref = coding_pos - 1;
         }
         else
         {
-            output_pointer = (pointer_pos << 4) | pointer_length;
+            output_pointer = (pointer_pos << pointer_length_width) | (pointer_length ? (pointer_length - 1) : 0);
             output_lookahead_ref = coding_pos;
         }
-        *((uint32_t *) (compressed_text + compressed_pointer)) = output_pointer;
+        *((uint16_t *) (compressed_text + compressed_pointer)) = output_pointer;
         compressed_pointer += 2;
         *(compressed_text + compressed_pointer++) = *(uncompressed_text + output_lookahead_ref);
         output_size += 3;
@@ -52,19 +56,22 @@ uint32_t lz77_compress (uint8_t *uncompressed_text, uint32_t uncompressed_size, 
 
 uint32_t lz77_decompress (uint8_t *compressed_text, uint8_t *uncompressed_text)
 {
-    uint8_t pointer_length;
-    uint16_t input_pointer, pointer_pos;
+    uint8_t pointer_length_width;
+    uint16_t input_pointer, pointer_length, pointer_pos, pointer_length_mask;
     uint32_t compressed_pointer, coding_pos, pointer_offset, uncompressed_size;
 
     uncompressed_size = *((uint32_t *) compressed_text);
-    compressed_pointer = 4;
+    pointer_length_width = *(compressed_text + 4);
+    compressed_pointer = 5;
+
+    pointer_length_mask = pow(2, pointer_length_width) - 1;
 
     for(coding_pos = 0; coding_pos < uncompressed_size; ++coding_pos)
     {
-        input_pointer = *((uint32_t *) (compressed_text + compressed_pointer));
+        input_pointer = *((uint16_t *) (compressed_text + compressed_pointer));
         compressed_pointer += 2;
-        pointer_pos = input_pointer >> 4;
-        pointer_length = input_pointer & 15;
+        pointer_pos = input_pointer >> pointer_length_width;
+        pointer_length = pointer_pos ? ((input_pointer & pointer_length_mask) + 1) : 0;
         if(pointer_pos)
             for(pointer_offset = coding_pos - pointer_pos; pointer_length > 0; --pointer_length)
                 uncompressed_text[coding_pos++] = uncompressed_text[pointer_offset++];
@@ -84,7 +91,7 @@ long fsize (FILE *in)
     return length;
 }
 
-uint32_t file_lz77_compress (char *filename_in, char *filename_out)
+uint32_t file_lz77_compress (char *filename_in, char *filename_out, size_t malloc_size, uint8_t pointer_length_width)
 {
     FILE *in, *out;
     uint8_t *uncompressed_text, *compressed_text;
@@ -99,9 +106,9 @@ uint32_t file_lz77_compress (char *filename_in, char *filename_out)
         return 0;
     fclose(in);
 
-    compressed_text = malloc(655360);
+    compressed_text = malloc(malloc_size);
 
-    compressed_size = lz77_compress(uncompressed_text, uncompressed_size, compressed_text);
+    compressed_size = lz77_compress(uncompressed_text, uncompressed_size, compressed_text, pointer_length_width);
 
     out = fopen(filename_out, "w");
     if(out == NULL)
@@ -152,7 +159,7 @@ int main (int argc, char const *argv[])
         return 0;
     printf("Original size: %ld\n", fsize(in));
     fclose(in);
-    printf("Compressed: %u\n", file_lz77_compress("./lz.c", "lz.c.z77"));
-    printf("Decompressed: %u", file_lz77_decompress("./lz.c.z77", "lz-2.c"));
+    for(uint8_t i = 1; i < 16; ++i)
+        printf("Compressed (%i): %u, decompressed: (%u)\n", i, file_lz77_compress("./lz.c", "lz.c.z77", 10000000, i), file_lz77_decompress("./lz.c.z77", "lz-2.c"));
     return 0;
 }
